a7deae1ba121269323aa90c871be700d195109f3
sbstjn <mail@sbstjn.com>
2016-10-22 23:13:32 +0200

    Add 'note' table to database-create.go

--- scripts/database-create.go
+++ scripts/database-create.go
@@ -33,14 +33,30 @@ func main() {
 
 	createAccount()
 	createToken()
+	createNote()
+}
+
+func createNote() {
+	// Search account tokens for parameter
+	rows, err := pool.Query(`CREATE TABLE note (
+		account integer NOT NULL,
+		text character varying(250) unique NOT NULL,
+		created timestamp DEFAULT current_timestamp NOT NULL
+  );`)
+	defer rows.Close()
+
+	if err != nil {
+		fmt.Println("Error select", err)
+		return
+	}
 }
 
 func createToken() {
 	// Search account tokens for parameter
 	rows, err := pool.Query(`CREATE TABLE token (
-    created timestamp DEFAULT current_timestamp NOT NULL,
-    token character varying(100) NOT NULL,
-    account integer NOT NULL
+		account integer NOT NULL,
+		token character varying(100) unique NOT NULL,
+		created timestamp DEFAULT current_timestamp NOT NULL
   );`)
 	defer rows.Close()
 
@@ -54,7 +70,7 @@ func createAccount() {
 	// Search account tokens for parameter
 	rows, err := pool.Query(`CREATE TABLE account (
     id serial primary key,
-    address varchar(100) NOT NULL,
+    address varchar(100) unique NOT NULL,
     verified boolean DEFAULT false NOT NULL,
     created timestamp DEFAULT current_timestamp NOT NULL,
     token character varying(100)
