35db95358d89f4ac95913f368d60af807c052b65
sbstjn <mail@sbstjn.com>
2016-11-23 21:23:52 +0100

    Update db and configuration handling

--- data/data.go
+++ data/data.go
@@ -27,11 +27,68 @@ import (
 var (
 	pool    *pgx.ConnPool
 	letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	// Queries holds the list of all needed sql queries
+	Queries = []map[string]string{
+		AccountQueries,
+		TokenQueries,
+		SubscriptionQueries,
+		NoteQueries,
+	}
 )
 
 // Pool configures the PostgreSQL pool
 func Pool(use *pgx.ConnPool) {
 	pool = use
+}
+
+// Setup creates the database structure
+func Setup(pool *pgx.Conn) {
+	pool.Exec(`
+		CREATE TABLE account(
+	    id serial primary key,
+	    address TEXT NOT NULL,
+	    created TIMESTAMP DEFAULT now() NOT NULL,
+	    verified BOOLEAN DEFAULT false NOT NULL
+		);
+
+		CREATE TABLE note(
+	    id serial primary key,
+	    account INTEGER NOT NULL,
+			text TEXT NOT NULL,
+	    created TIMESTAMP DEFAULT now() NOT NULL
+		);
+
+		CREATE TABLE subscription(
+	    id serial primary key,
+	    account INTEGER NOT NULL,
+	    created TIMESTAMP DEFAULT now() NOT NULL,
+	    stripeid TEXT NOT NULL,
+	    active BOOLEAN DEFAULT false
+		);
+
+		CREATE TABLE token(
+	    id serial primary key,
+	    account INTEGER NOT NULL,
+	    text TEXT NOT NULL,
+	    created TIMESTAMP DEFAULT now() NOT NULL,
+	    type INTEGER DEFAULT 1 NOT NULL,
+	    active BOOLEAN DEFAULT true
+		);
+
+		CREATE UNIQUE INDEX account_id_uindex ON account (id);
+		CREATE UNIQUE INDEX account_address_uindex ON account (address);
+
+		ALTER TABLE subscription ADD FOREIGN KEY (account) REFERENCES account (id) on delete cascade;
+		CREATE UNIQUE INDEX subscription_id_uindex ON subscription (id);
+		CREATE UNIQUE INDEX "subscription_stripeID_uindex" ON subscription (stripeid);
+
+		ALTER TABLE token ADD FOREIGN KEY (account) REFERENCES account (id) on delete cascade;
+		CREATE UNIQUE INDEX token_id_uindex ON token (id);
+
+		ALTER TABLE note ADD FOREIGN KEY (account) REFERENCES account (id) on delete cascade;
+		CREATE UNIQUE INDEX note_id_uindex ON note (id);
+	`)
 }
 
 // Get returns `n` random characters
